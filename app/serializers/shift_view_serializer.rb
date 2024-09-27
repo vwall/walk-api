19206eefb220093a89cf0008ac2d2861e611b3e5
Vincent Waller <vrwaller@gmail.com>
2024-09-27 15:11:20 -0700

    Unused file

--- app/serializers/shift_view_serializer.rb
+++
@@ -1,7 +0,0 @@
-class ShiftViewSerializer < BaseSerializer
-  attributes attributes :id, :day, :walker_id
-
-  attribute :name_with_email do |resource|
-    "#{resource.name}: #{resource.email}"
-  end
-end
